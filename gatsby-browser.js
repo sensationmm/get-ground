cb2bc9dfae773a8988b954d4c9580aafe9c9c13d
ravinpatel <ravinp1337@gmail.com>
2019-04-29 10:34:07 +0100

    RP: configured gatsby to use both redux and react-provider. Redux is SSR

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,14 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
-// import ReduxWrapper from './src/state/ReduxWrapper';
 import React from "react"
+import { ReduxWrapper } from './src/state/ReduxWrapper'
 
 import { WindowSizeProvider } from "./src/context/WindowSizeContext"
 
-export const wrapRootElement = ({ element }) => (
-    <WindowSizeProvider>{element}</WindowSizeProvider>
-)
+export const wrapRootElement =  ({ element }) => {
+  return (
+    <ReduxWrapper>
+      <WindowSizeProvider>
+        {element}
+      </WindowSizeProvider>
+    </ReduxWrapper>
+  )
+}
