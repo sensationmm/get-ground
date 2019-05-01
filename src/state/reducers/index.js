db8628583917bf0f4123819287cac966f0684f83
ravinpatel <ravinp1337@gmail.com>
2019-05-01 10:47:17 +0100

    RP: removed ui reducer and actions from redux state

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,6 +1,4 @@
 import { combineReducers } from 'redux';
-// import app from '../app';
-// import { ui } from './reducers/ui'
 import { loader } from './loader'
 
 export default combineReducers({ loader });
