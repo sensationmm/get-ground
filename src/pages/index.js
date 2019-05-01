fdd3eb42019e9b2180e57f891622359cc5989fa6
ravinpatel <ravinp1337@gmail.com>
2019-05-01 11:05:22 +0100

    RP: added home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 import Layout from '../components/layout/layout'
 import SEO from '../components/seo'
+import Select from '../components/_form/Select/Select';
 
-const IndexPage = () => (
-    <Layout>
-      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      <h1>Home</h1>
-    </Layout>
-)
+import '../styles/pages/home.scss';
+
+const IndexPage = () => {
+
+  const testOptions = [
+    {
+      name: 'test1',
+      value: 'test1'
+    },
+    {
+      name: 'test2',
+      value: 'test2'
+    }
+  ];
+
+  const buildTest = testOptions.map((test, index) => {
+    return (
+      <option key={`country-${index}`} value={test.value}>{test.name}</option>
+    );
+  });
+
+  return (
+    <Fragment>
+      <SEO title="GetGround" keywords={[`gatsby`, `application`, `react`]} />
+      <Layout>
+        <div data-test="container-home" className="home" role="homepage">
+        Home
+        <Select
+          classes="country-select"
+          defaultOptionText="Select Country"
+          options={buildTest}
+          label="Country"
+          onChange={/* istanbul ignore next */ (e) => {}}
+        />
+      </div>
+      </Layout>
+    </Fragment>
+  )
+}
 
 export default IndexPage
