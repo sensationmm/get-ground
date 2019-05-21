0182e9145ac3e9d982673f59f58374a31763bc18
Kevin Reynolds <kevin.reynolds@nimbletank.com>
2019-05-21 16:05:24 +0100

    move form utils into redux

---
+++ src/state/reducers/activeCompany.js
@@ -0,0 +1,8 @@
+const initialState = 0;
+
+export const activeCompany = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
---
+++ src/state/reducers/companies.js
@@ -0,0 +1,8 @@
+const initialState = [];
+
+export const companies = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
