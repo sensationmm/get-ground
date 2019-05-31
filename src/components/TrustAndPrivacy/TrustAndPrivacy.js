353803e9e640460b7c013c19d704aadad4ea5a2c
ravinpatel <ravinp1337@gmail.com>
2019-05-31 15:43:10 +0100

    RP: trust and privacy

--- src/components/TrustAndPrivacy/TrustAndPrivacy.js
+++ src/components/TrustAndPrivacy/TrustAndPrivacy.js
@@ -1,15 +1,87 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next';
+import classNames from 'classnames';
+
+import Button from 'src/components/_buttons/Button/Button'
+import FCA from 'src/assets/images/fca.png'
+import Geovation from 'src/assets/images/geovation.png'
+import LandReg from 'src/assets/images/land-reg.png'
+import ICO from 'src/assets/images/ico.png'
 
 import './trust-and-privacy.scss'
-import { useTranslation } from 'react-i18next';
 
 const TrustAndPrivacy = () => {
-  const [t] = useTranslation();
+  const [t, i18n] = useTranslation();
+  const [show, toggleShow] = useState(false);
+
+  const infoContent = i18n.t('trustAndPrivacy.sections', { returnObjects: true });
+  const infoConfig = {
+    infos: [
+      {
+        'title': infoContent['info0'].title,
+        'copy': infoContent['info0'].copy
+      },
+      {
+        'title': infoContent['info1'].title,
+        'copy': infoContent['info1'].copy
+      },
+      {
+        'title': infoContent['info2'].title,
+        'copy': infoContent['info2'].copy
+      },
+      {
+        'title': infoContent['info3'].title,
+        'copy': infoContent['info3'].copy
+      },
+      {
+        'title': infoContent['info4'].title,
+        'copy': infoContent['info4'].copy
+      },
+      {
+        'title': infoContent['info5'].title,
+        'copy': infoContent['info5'].copy
+      },
+      {
+        'title': infoContent['info6'].title,
+        'copy': infoContent['info6'].copy
+      },
+      {
+        'title': infoContent['info7'].title,
+        'copy': infoContent['info7'].copy
+      },
+    ]
+  };
 
   return (
-    <div className="trustAndPrivacy">
+    <div className={classNames(
+      'trustAndPrivacy',
+      { 'read-more': show },
+    )}>
       <h1 className="trustAndPrivacy-title">{t('trustAndPrivacy.title')}</h1>
       <p className="trustAndPrivacy-subTitle">{t('trustAndPrivacy.subTitle')}</p>
+      {show
+        ?
+          infoConfig.infos.map((info, idx) => (
+            <div key={`${idx} + ${info.title}`} className="trustAndPrivacy-info">
+              <div className="trustAndPrivacy-info-title">{info.title}</div>
+              <div className="trustAndPrivacy-info-copy">{info.copy}</div>
+            </div>
+          ))
+        :
+        <Button
+          classes="trustAndPrivacy-livechat"
+          data-test="livechat-button"
+          liveChat
+          label={t('trustAndPrivacy.cta')}
+          onClick={() => toggleShow(!show)}
+        />
+      }
+      <div className="trustAndPrivacy-logos">
+        <img src={FCA} />
+        <img src={Geovation} />
+        <img src={LandReg} />
+        <img src={ICO} />
+      </div>
     </div>
   )
 }
--- src/pages/what-we-do.js
+++ src/pages/what-we-do.js
@@ -1,9 +1,9 @@
 import React, { Fragment } from 'react';
 import { useTranslation } from 'react-i18next';
-// import ImageFull from 'src/components/ImageFull/ImageFull'
+
 import ContactUs from 'src/components/ContactUs/ContactUs'
 
-// import house from 'src/assets/images/house-landing.svg'
+import HandsAndPhone from 'src/assets/images/hands-and-phone.svg'
 import Layout from 'src/components/Layout/Layout'
 import TrustAndPrivacy from 'src/components/TrustAndPrivacy/TrustAndPrivacy'
 
@@ -16,7 +16,7 @@ const WhatWeDo = () => {
     <Fragment>
       <Layout>
         <div data-test="container-whatWeDo" className="whatWeDo" role="company-design">
-          {/* <ImageFull src={house} alt="house" data-test="whatWeDo-img" /> */}
+          <img className="whatWeDo-img" src={HandsAndPhone} alt="Hands holding Phone" data-test="whatWeDo-img" />
           <h1 data-test="whatWeDo-title" >{t('whatWeDo.title')}</h1>
           <div className="whatWeDo-content">
             <p data-test="whatWeDo-content-first" >{t('whatWeDo.content.first')}</p>
