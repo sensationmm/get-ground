c25e5b390e8465fbf6b14f95a8d5b9afd68792a8
ravinpatel <ravinp1337@gmail.com>
2019-06-05 14:06:05 +0100

    RP: line and steps

--- src/pages/how-it-works.js
+++ src/pages/how-it-works.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next';
 
 import Layout from 'src/components/Layout/Layout'
 import ContactUs from 'src/components/ContactUs/ContactUs'
 import Bounce from 'react-reveal/Bounce'
+import useWindowScroll from 'react-use/lib/useWindowScroll';
 
 import Step1 from 'src/assets/images/HowItWorks-step1.svg'
 import Step2 from 'src/assets/images/HowItWorks-step2.svg'
@@ -11,12 +12,19 @@ import Step3 from 'src/assets/images/HowItWorks-step3.svg'
 import Step4 from 'src/assets/images/HowItWorks-step4.svg'
 import Step5 from 'src/assets/images/HowItWorks-step5.svg'
 
-import CoinAndHand from 'src/assets/images/coins-and-hand.svg'
+import Roof from 'src/assets/images/roof.svg'
 
 import 'src/styles/pages/how-it-works.scss'
 
 const HowItWorks = () => {
   const [t, i18n] = useTranslation();
+  const { y } = useWindowScroll() ;
+
+  console.log((y /window.innerHeight) * 100);
+
+  const height = () => {
+
+  }
 
   const stepContent = i18n.t('howItWorks.sections', { returnObjects: true });
   const stepConfig = {
@@ -24,53 +32,53 @@ const HowItWorks = () => {
       {
         'title': stepContent['step1'].title,
         'copy': stepContent['step1'].copy,
-        'image': CoinAndHand
+        'image': Step1
       },
       {
         'title': stepContent['step2'].title,
         'copy': stepContent['step2'].copy,
-        'image': CoinAndHand
+        'image': Step2
       },
       {
         'title': stepContent['step3'].title,
         'copy': stepContent['step3'].copy,
-        'image': CoinAndHand
+        'image': Step3
       },
       {
         'title': stepContent['step4'].title,
         'copy': stepContent['step4'].copy,
-        'image': CoinAndHand
+        'image': Step4
       },
       {
         'title': stepContent['step5'].title,
         'copy': stepContent['step5'].copy,
-        'image': CoinAndHand
+        'image': Step5
       }
     ]
   };
 
-
+  const lineHeight = (y/window.innerHeight) * 100
 
   return (
     <Layout>
       <div className="how-it-works">
-        <img className="pricing-img" src={CoinAndHand} alt="coins in a jar" data-test="pricing-img" />
+        <img className="how-it-works-img" src={Roof} alt="roof of house" data-test="how-it-works-img" />
         <h1 className="how-it-works-title">{t('howItWorks.title')}</h1>
-        <ul>
-        {stepConfig.steps.map((step, idx) => (
-          <div key={`${idx} + ${step.title}`}>
-            <Bounce delay={1000}>
-              <div className="how-it-works-step">
-                <img src={CoinAndHand}/>
-                <li className="how-it-works-step-li">
-                  <div className="how-it-works-step-bullet"></div>
-                  <p className="how-it-works-step-title">{step.title}</p>
-                  <p className="how-it-works-step-copy">{step.copy}</p>
-                </li>
-              </div>
-            </Bounce>
-          </div>
-        ))}
+          <ul>
+          {stepConfig.steps.map((step, idx) => (
+            <div key={`${idx} + ${step.title}`} className="how-it-works-steps">
+              <Bounce>
+                <div className="how-it-works-step">
+                  <img src={step.image}/>
+                  <li className="how-it-works-step-li">
+                    <p className="how-it-works-step-title">{step.title}</p>
+                    <p className="how-it-works-step-copy">{step.copy}</p>
+                  </li>
+                </div>
+              </Bounce>
+            </div>
+          ))}
+          <div className="how-it-works-step-verticalLine" style={{height: lineHeight > 88 ? `77.5%` : `${lineHeight}%`}}></div>
         </ul>
         <ContactUs data-test="pricing-contact-us"/>
       </div>
