3fc8ecbb8fb32f82651c8937ae1586ab84ac30b1
ravinpatel <ravinp1337@gmail.com>
2019-06-07 13:25:22 +0100

    RP: nearly

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,24 +1,40 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import './table.scss'
 
-const Table = ({ sections, classes }) => {
+const Table = ({ sections, classes, images }) => {
   return (
-    <div className={`table ${classes}`}>
+  <>
+    <div className={classNames(
+      'table',
+      classes,
+      {'images': images},
+    )}>
       {sections.map((section, idx) => (
         <div key={`${section} + ${idx}`} className="table-section">
-          <p className="table-section-copy">{section}</p>
+          { images ?
+          <img className="table-section-img" src={section.img}/>
+          :
+          <>
+           {section.img && <img className="table-section-img" src={section.img}/>}
+            <p className="table-section-copy">{section.copy}</p>
+          </>
+          }
           <div className="table-section-line"/>
         </div>
       ))}
     </div>
+    </>
   )
 }
 
 Table.propTypes = {
   sections: PropTypes.array,
-  classes: PropTypes.string
+  classes: PropTypes.string,
+  images: PropTypes.bool,
+  header: PropTypes.string
 }
 
 export default Table;
--- src/pages/advantages.js
+++ src/pages/advantages.js
@@ -1,6 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Layout from 'src/components/Layout/Layout'
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next'
+
+// import ReactSwipe from 'react-swipe';
+// import { Carousel } from 'react-responsive-carousel';
+// import SwipeableViews from 'react-swipeable-views';
+
+import Piggy from 'src/assets/images/piggy.svg'
+import whiteTick from 'src/assets/images/white-tick.svg'
+import orangeCross from 'src/assets/images/orange-cross.svg'
+import classNames from 'classnames';
 
 import ContactUs from 'src/components/ContactUs/ContactUs'
 import Footer from 'src/components/Footer/Footer';
@@ -11,6 +20,7 @@ import 'src/styles/pages/advantages.scss'
 
 const Advantages = () => {
   const [t, i18n] = useTranslation()
+  const [taxTableIndex, toggleTableIndex] = useState(0)
   const [showTaxTable, toggleTaxTable] = useState(false);
   // const [showCompanyTable, toggleCompanyTable] = useState(false);
   const ukTaxObj = i18n.t('advantages.tax', { returnObjects: true });
@@ -32,30 +42,197 @@ const Advantages = () => {
   ]
 
   const sections = [
-    'Same taxes on property purchase',
-    'Deduct mortgage interest from your UK tax bill',
-    'Lower capital gains tax (CGT)',
-    'Save on stamp duty and land tax (SDLT) when you sell',
-    'Efficiently take income from your property'
+    {
+      copy: 'Same taxes on property purchase',
+      img: ''
+    },
+    {
+      copy: 'Deduct mortgage interest from your UK tax bill',
+      img: ''
+    },
+    {
+      copy: 'Lower capital gains tax (CGT)',
+      img: ''
+    },
+    {
+      copy: 'Save on stamp duty and land tax (SDLT) when you sell',
+      img: ''
+    },
+    {
+      copy: 'Efficiently take income from your property',
+      img: ''
+    }
   ]
 
-  const sectionsFeed = [
-    '',
-    'Same',
-    'Deduct',
-    'Lower',
-    'Save',
-    'Efficiently'
+  const smallFeed = [
+    {
+      copy: '',
+      img: ''
+    },
+    {
+      copy: '',
+      img: orangeCross
+    },
+    {
+      copy: '',
+      img: orangeCross
+    },
+    {
+      copy: '',
+      img: orangeCross
+    },
+    {
+      copy: '',
+      img: orangeCross
+    }
   ]
+
+  const smallFeed2 = [
+    {
+      copy: '',
+      img: ''
+    },
+    {
+      copy: '',
+      img: whiteTick
+    },
+    {
+      copy: '',
+      img: whiteTick
+    },
+    {
+      copy: '',
+      img: whiteTick
+    },
+    {
+      copy: '',
+      img: whiteTick
+    }
+  ]
+
+  const feed1 = [
+    {
+      copy: '',
+      img: ''
+    },
+    {
+      copy: 'Replaced with a basic rate credit',
+      img: orangeCross
+    },
+    {
+      copy: '18% or 28% CGT tax',
+      img: orangeCross
+    },
+    {
+      copy: '',
+      img: orangeCross
+    },
+    {
+      copy: 'Either income tax or capital gains tax',
+      img: orangeCross
+    }
+  ]
+
+  const feed2 = [
+    {
+      copy: 'same taxes (e.g SDLT) as personal ownership if you rent out your property to 3rd parties',
+      img: ''
+    },
+    {
+      copy: '',
+      img: whiteTick
+    },
+    {
+      copy: '10% or 20% CGT tax for sale of shares',
+      img: whiteTick
+    },
+    {
+      copy: 'No SDLT when buying shares',
+      img: whiteTick
+    },
+    {
+      copy: 'Choose between capital repayments or dividends',
+      img: whiteTick
+    }
+  ]
+
+  const showTable = (index) => {
+    if(index === 0) {
+      return (
+        <div className="answers tax">
+          <Table classes="right small" sections={smallFeed} images />
+          <Table classes="right small blue" sections={smallFeed2} images />
+        </div>
+      )
+    }
+
+    if (index === 1) {
+      return (
+        <Table classes="answers right tax" sections={feed1} />
+      )
+    }
+
+    if (index === 2) {
+      return (
+        <Table classes="answers right blue tax" sections={feed2} />
+      )
+    }
+  }
+
+  useEffect(() => {
+    if (showTaxTable) {
+      const _T = document.querySelector('.answers.tax');
+      let ts = 0;
+      _T.addEventListener('touchstart', function(e) {
+        ts = e.changedTouches[0].clientX
+      })
+
+      _T.addEventListener('touchend', function(e) {
+        const te = e.changedTouches[0].clientX
+        if (ts < te ) {
+          const idx = taxTableIndex === 0 ? 2 : taxTableIndex - 1
+          toggleTableIndex(idx)
+        } else {
+          const idx = taxTableIndex === 2 ? 0 : taxTableIndex + 1
+          toggleTableIndex(idx)
+        }
+      })
+    }
+  })
+
+  const expandedTax = () => (
+    <div className="advantages-uk-tax-more-expanded">
+      <Table sections={sections} />
+        {showTable(taxTableIndex)}
+      <div className="advantages-uk-tax-more-expanded-bullet">
+        <span
+          className={classNames({ 'active' : taxTableIndex === 0 })}
+          onClick={() => toggleTableIndex(0)}
+        />
+        <span
+          className={classNames({ 'active' : taxTableIndex === 1 })}
+          onClick={() => toggleTableIndex(1)}
+        />
+        <span
+          className={classNames({ 'active' : taxTableIndex === 2 })}
+          onClick={() => toggleTableIndex(2)}
+        />
+      </div>
+      {/* <p className="advantages-uk-tax-more-expanded-info">None of the information contained here, nor anywhere in this presentation contributes advice</p> */}
+    </div>
+  )
+
+
 
   return (
     <Layout>
       <div className="advantages">
+        <img className="advantages-img" src={Piggy} alt="Piggy bank"/>
         <h1>{t('advantages.title')}</h1>
         <div className="advantages-uk-tax">
         <h3 className="advantages-uk-tax-title">{t('advantages.tax.title')}</h3>
-        {/* {showTaxTable ?
-          <Table sections={sections} />
+        {showTaxTable ?
+          expandedTax()
           :
           <>
             <ul>
@@ -67,16 +244,7 @@ const Advantages = () => {
             </ul>
             <Button classes="advantages-btn chat" fullWidth label={t('advantages.cta')} onClick={() => toggleTaxTable(!showTaxTable)}/>
           </>
-        } */}
-        <div className="advantages-uk-tax-more-expanded">
-          <div className="advantages-uk-tax-more-expanded-table-first">
-            <Table sections={sections} />
-          </div>
-          <div className="advantages-uk-tax-more-expanded-table-second">
-            <Table classes="small" sections={sectionsFeed} />
-            <Table classes="small" sections={sectionsFeed} />
-          </div>
-        </div>
+        }
         </div>
         <div className="advantages-company">
         <h3 className="advantages-company-title">{t('advantages.company.title')}</h3>
