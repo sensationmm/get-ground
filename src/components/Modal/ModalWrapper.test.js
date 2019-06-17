2033d53e250e5129f2032d271f8d8dd8bb5c3465
luke tuckey <luketuckey@hotmail.com>
2019-06-17 09:47:46 +0100

    added another unit test for ModalWrapper

--- src/components/Modal/ModalWrapper.test.js
+++ src/components/Modal/ModalWrapper.test.js
@@ -1,6 +1,7 @@
 import { setup, findByTestAttr } from 'src/test-utils/test-utils';
 
 import ModalWrapper from './ModalWrapper';
+import Modal from './Modal';
 
 describe('<ModalWrapper />', () => {
   let wrapper;
@@ -16,5 +17,9 @@ describe('<ModalWrapper />', () => {
   test('renders without error', () => {
     const component = findByTestAttr(wrapper, 'component-modal-wrapper');
     expect(component.length).toBe(1);
+  });
+
+  test('has Modal component as child ', () => {
+    expect(Modal.length).toBe(1);
   });
 });
