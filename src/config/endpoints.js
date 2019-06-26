ba2d5cffef992c18e4822d2bcbf17fe2a668f1a4
luke tuckey <luketuckey@hotmail.com>
2019-06-26 14:37:53 +0100

    address finder url now https

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -3,5 +3,5 @@ const addressNowKey = 'jy73-yx25-bu91-gk98';
 export const stripeKey = 'pk_test_IekVa77loZrt2oMKeUXuHek7';
 
 export const API = `https://staging-backend-236514.appspot.com/api/${version}`;
-export const addressNow = `http://api.addressnow.co.uk/js/addressnow-2.20.min.js?key=${addressNowKey}`;
+export const addressNow = `https://api.addressnow.co.uk/js/addressnow-2.20.min.js?key=${addressNowKey}`;
 export const filePath = 'https://staging-backend-236514.appspot.com/';
