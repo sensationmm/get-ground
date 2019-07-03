2571b10e0b9b1eaa081e4af7f77b43f4beb18cc0
ravinpatel <ravinp1337@gmail.com>
2019-07-03 16:17:41 +0100

    RP: changed input type to number

--- src/components/_form/InputPhone/InputPhone.js
+++ src/components/_form/InputPhone/InputPhone.js
@@ -20,12 +20,12 @@ import './input-phone.css';
  * @return {JSXElement} InputPhone
  */
 const InputPhone = (props) => {
-  const { 
-    id, 
-    label, 
-    onChange, 
-    value, 
-    validate, 
+  const {
+    id,
+    label,
+    onChange,
+    value,
+    validate,
     readOnly,
     note,
     error,
@@ -65,7 +65,7 @@ const InputPhone = (props) => {
 
         <input
           data-test="phone-number"
-          type="text"
+          type="number"
           id={id}
           onBlur={onBlur}
           onChange={(e) => onChange(`(${country})${e.target.value}`)}
