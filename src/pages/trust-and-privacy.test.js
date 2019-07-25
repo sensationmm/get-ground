19e8b669d70fd4a126c0e9465c6e450a1b5646c7
Kevin Reynolds <kevin.reynolds@nimbletank.com>
2019-07-25 11:28:57 +0100

    Trust and Privacy

---
+++ src/pages/trust-and-privacy.test.js
@@ -0,0 +1,15 @@
+import TrustAndPrivacy from './trust-and-privacy'
+import { setup, findByTestAttr } from 'src/test-utils/test-utils';
+
+describe('trust and privacy page', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = setup(TrustAndPrivacy)
+  })
+
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'container-trustAndPrivacy');
+    expect(component.length).toBe(1);
+  });
+})
